b1de95b5caf04f53f32adc5912c52eb51d39edcd
Sanjay Surawar <sanjay.surawar@globant.com>
2017-12-07 14:42:34 +0530

    second time code pushing

--- src/app/components/flag/flag.component.ts
+++ src/app/components/flag/flag.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlagComponent implements OnInit {
 
+  flatNotifList: any[];
   constructor() { }
 
   ngOnInit() {
+    this.flatNotifList = [
+      {voidNumber: 'WOID 1144332', flatDescrip: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum printing and typesetting industry. Lorem Ipsum.', flatTime: '10:27 am'},
+      {voidNumber: 'WOID 1244332', flatDescrip: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum printing and typesetting industry.', flatTime: '11:27 am'},
+      {voidNumber: 'WOID 1344332', flatDescrip: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum printing and typesetting industry. Lorem Ipsum.', flatTime: '11:48 am'}
+    ]
   }
 
 }
